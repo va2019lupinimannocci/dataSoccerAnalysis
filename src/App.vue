9abf13e60efa0e233bd57cd7c126ddf943807cdf
Lorenzo <mannocci.lore@gmail.com>
2019-07-29 17:41:44 +0200

    Sfondo HTML

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,22 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Visualizzazione squadre prova</b-nav-item>
+          <b-nav-item href="#">Visualizzazione squadre</b-nav-item>
           <b-nav-item href="#">Visualizzazione giocatori</b-nav-item>
           <b-nav-item href="#">Visualizzazione partita</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <div>
-      <div>
-        <h4>Top and Bottom</h4>
-        <b-card-group deck>
-          <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-top>
-            <b-card-text>
-              Some quick example text to build on the card and make up the bulk of the card's content.
-            </b-card-text>
-          </b-card>
-
-          <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-bottom>
-            <b-card-text>
-              Some quick example text to build on the card and make up the bulk of the card's content.
-            </b-card-text>
-          </b-card>
-        </b-card-group>
-      </div>
-      <div class="mt-4">
-        <h4>Left and Right (or Start and End)</h4>
-        <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
-          <b-card-text>
-            Some quick example text to build on the card and make up the bulk of the card's content.
-          </b-card-text>
-        </b-card>
-      </div>
-    </div>
+    <Visualizzazione_Partita></Visualizzazione_Partita>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line camelcase
+import Visualizzazione_Partita from '@/components/Visualizzazione_Partita'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {Visualizzazione_Partita}
 }
 </script>
 
---
+++ src/components/Visualizzazione_Partita.vue
@@ -0,0 +1,72 @@
+<template>
+    <b-container>
+        <b-row class="field col-up-offset-1">
+            <b-col cols="6">
+                <h3>Campo da gioco</h3>
+                <div style="height:600px; background-color: #C8FFBD"></div>
+            </b-col>
+            <b-col>
+                <h3>Filtri sui dati</h3>
+                <div style="height:600px; background-color: #C8FFBD">
+                    <b-form-group  class="text-align col-up-offset-1 col-lf-offset-1"  label="Seleziona i dati che vuoi visualizzare">
+                        <b-form-checkbox-group
+                                v-model=selected
+                                :options="eventType.options"
+                                name="eventType"
+                                buttons
+                                stacked
+                        ></b-form-checkbox-group>
+                    </b-form-group>
+                </div>
+            </b-col>
+        </b-row>
+        <b-row class="time col-up-offset-1 col-bt-offset-1">
+            <b-col>
+                <h5>Slider tempo partita</h5>
+                <div style="height:50px; background-color: #C8FFBD"></div>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+/*
+import Vue from 'vue'
+import crossfilter from 'crossfilter'
+
+// crossfilter data management
+let cf; // crossfilter instance
+let dYear; // dimension for Year
+let dRecordType; // dimension for RecordType
+let dVesselType; // dimension for VesselType
+*/
+
+export default {
+  name: 'Visualizzazione_Partita',
+  data () {
+    return {
+      eventType: {
+        value: 'Goal',
+        options: ['Goal', 'Passaggio', 'Offside']
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .col-up-offset-1{
+        margin-top: 1em;
+    }
+    .col-bt-offset-1 {
+        margin-bottom: 1em;
+    }
+
+    .col-lf-offset-1 {
+        margin-left: 1em;
+    }
+
+    .text-align {
+        text-align: left;
+    }
+</style>
