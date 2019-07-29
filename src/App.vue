3470ca61cf44aa9d4e3a7fde18b89c552f14ba9a
Lorenzo <mannocci.lore@gmail.com>
2019-07-29 15:14:41 +0200

    Visualizzazione partite prova

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Visualizzazione squadre</b-nav-item>
+          <b-nav-item href="#">Visualizzazione squadre prova</b-nav-item>
           <b-nav-item href="#">Visualizzazione giocatori</b-nav-item>
           <b-nav-item href="#">Visualizzazione partita</b-nav-item>
         </b-navbar-nav>
